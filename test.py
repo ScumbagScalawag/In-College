18f787dde527cc7b33f94ca5975ecf9d4a5e5f42
AndrewManyak <andrewmanyak1@gmail.com>
2023-09-19 13:47:59 -0400

    Test Under construct. + Registration

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import pytest
 from main import printNewAccountScreen, saveUser
 import json
 
-def test_max_users(capfd, monkeypatch):
+def testCreateAccountOver5(capfd, monkeypatch):
     with open("user_file.json", "w") as f:
         f.write("{}")
-    # Load 5 accounts to test maximum feature
+    # Load 5 accounts to Json
     accounts = {
         "Andrew": "Valid123!",
         "Candy": "Valid123!",
@@ -13,51 +13,18 @@ def test_max_users(capfd, monkeypatch):
         "Sandy": "Valid123!",
         "fishy": "Valid123!"
     }
+    #Confirm there is 5 accounts
     for username, password in accounts.items():
         saveUser(username, password)
     with open("user_file.json", "r") as f:
         users_data = json.load(f)
     assert len(users_data) == 5
     # Test 6th account
-    input_values = ["user6", "Password1@", "Password1@"]
-    input_generator = iter(input_values)
-
+    input_generator = iter(["user6", "P@ssword1", "P@ssword1"])
     monkeypatch.setattr('builtins.input', lambda: next(input_generator))
-
     try:
         printNewAccountScreen()
     except StopIteration:
         pass
-    # Captures what is printed
     captured = capfd.readouterr()
     assert "All permitted accounts have been created, please come back later" in captured.out
-
-
-def test_create_account_when_less_than_5_users(monkeypatch, capfd):
-    # Clear the user database before starting the test
-    with open("user_file.json", "w") as f:
-        f.write("{}")
-
-    # Emulate a sequence of inputs
-    input_generator = iter(["newUser", "Password1@", "Password1@"])
-
-    # Use monkeypatch to replace the built-in input function with our generator
-    monkeypatch.setattr('builtins.input', lambda: next(input_generator))
-
-    # Handle the StopIteration exception when our generator runs out of items
-    try:
-        printNewAccountScreen()
-    except StopIteration:
-        pass
-
-    # Capture the standard output and error for assertion
-    captured = capfd.readouterr()
-
-    # Verify the main menu is printed
-    main_menu = (
-        "*** Main Menu ***\n"
-        "1 - Search for a job\n"
-        "2 - Find someone that you know\n"
-        "3 - Learn a skill\n"
-    )
-    assert main_menu in captured.out
