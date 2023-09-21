82716c0103d90f5fc0b31cb3bb5fe2a58f81be04
Noah McIvor <$GIT_USER_EMAIL>
2023-09-20 23:46:44 -0400

    test3 failing but imports are correct

--- tests/test3.py
+++ tests/test3.py
@@ -1,16 +1,20 @@
 import pytest
-from main import printNewAccountScreen
-import json
-@pytest.mark.parametrize("password_input", [
-    "small",  # Too short
-    "tkskadkkasdasdlsldlas",  # Too long
-    "lowercase123!",  # No capital letter
-    "UPPERCASEONLY!",  # No digit
-    "NoSpecialChars1"  # No special character
-])
+from pages.new_user_account import printNewAccountScreen
+
+
+@pytest.mark.parametrize(
+    "password_input",
+    [
+        "small",  # Too short
+        "tkskadkkasdasdlsldlas",  # Too long
+        "lowercase123!",  # No capital letter
+        "UPPERCASEONLY!",  # No digit
+        "NoSpecialChars1",  # No special character
+    ],
+)
 def test_invalid_password_criteria(password_input, monkeypatch, capfd):
     input_generator = iter(["username", password_input, password_input])
-    monkeypatch.setattr('builtins.input', lambda: next(input_generator))
+    monkeypatch.setattr("builtins.input", lambda: next(input_generator))
     try:
         printNewAccountScreen()
     except StopIteration:
