5c75f115f1968661a80c1deb17b3b6694008b630
Noah McIvor <$GIT_USER_EMAIL>
2023-09-23 23:06:48 -0400

    initial screen: isolated options and testimonial

--- pages/initial_screen.py
+++ pages/initial_screen.py
@@ -8,25 +8,12 @@ from pages.new_user_account import printNewAccountScreen
 def printInitialScreen():
     clearScreen()
     while True:
-        print("*** Welcome to InCollege ***")
-        print("Here is a story from one of our users:")
-        print(
-            """Hi, my name is Jordan and I have been a huge fan of In-College ever since 
-I learned about it through my university. It has been so nice to finally have
-a website dedicated to college students, and to be able to connect with all of 
-my friends and peers. I even found my first internship through In-College and will
-be starting there next month! I can't thank In-College enough for helping me 
-succeed in my early career development. """
-        )
-        print("Press any button to continue")
-        tempInput = input("")
+        printTestimonialPage()
         clearScreen()
 
-        print("*** Welcome to InCollege ***")
-        print("1 - Login as existing user")
-        print("2 - Create a new InCollege account")
-        print("3 - Find InCollege users")
-        print("X - Close Program")
+        options = initialScreenOptionsList
+        for option in options:
+            print(option)
 
         userInput = input("")
 
@@ -45,3 +32,31 @@ succeed in my early career development. """
         else:
             print('Invalid selection please input "1" or "2"')
     return 0
+
+
+testimonialOutputList = [
+    "*** Welcome to InCollege ***",
+    "Here is a story from one of our users:",
+    """Hi, my name is Jordan and I have been a huge fan of In-College ever since 
+I learned about it through my university. It has been so nice to finally have
+a website dedicated to college students, and to be able to connect with all of 
+my friends and peers. I even found my first internship through In-College and will
+be starting there next month! I can't thank In-College enough for helping me 
+succeed in my early career development. """,
+    "Press any button to continue",
+]
+
+
+def printTestimonialPage():
+    for output in testimonialOutputList:
+        print(output)
+    tempInput = input("")
+    return
+
+initialScreenOptionsList = [
+        "*** Welcome to InCollege ***",
+        "1 - Login as existing user",
+        "2 - Create a new InCollege account",
+        "3 - Find InCollege users",
+        "X - Close Program",
+    ]
