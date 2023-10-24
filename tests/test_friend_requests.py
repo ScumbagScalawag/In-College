56f35e8625f8c7503bb9a94f1b8ebbc7b9dde946
adam-k4 <adamk4@usf.edu>
2023-10-24 01:33:09 -0400

    Put parameterization back since it doesn't work all at once even without parameterization

--- tests/test_friend_requests.py
+++ tests/test_friend_requests.py
@@ -6,64 +6,42 @@ from pages.login import printLoginScreen
 from tests.shared import fourAccounts, singleUser
 
 
-# Commented this out because for some reason database was acting up when parameterized test ran multiple times
-#########################################################
-# @pytest.mark.parametrize(
-#     "mock_input,responses,expectedReturn",
-#     [
-#         (
-#             ["y",""],  # mock input
-#             [
-#                 "You are now friends with sillyBoi",  # responses
-#             ],
-#             None,  # expected return
-#         ),
-#         (
-#             ["n",""],
-#             [
-#                 "You have declined the friend request from sillyBoi",
-#             ],
-#             None,
-#         ),
-#         (
-#             ["X",""],
-#             [
-#                 "Invalid input. Please enter 'y' or 'n': ",
-#             ],
-#             None,
-#         ),
-#     ],
-#     ids=["Accept", "Decline", "Invalid"],
-# )
-# def test_manageFriendRequest(mock_input, responses, expectedReturn, monkeypatch, capfd):
-#     userDB = UserDatabase()
-#     userDB.addUserDictList(fourAccounts)
-#     print(userDB.getUserDictList())
-#     testUser = userDB.getUser("dummy")
-#     # expectedReturn = None
-#     input_generator = iter(mock_input)
-#     monkeypatch.setattr("builtins.input", lambda _: next(input_generator))
-
-#     try:
-#         assert manage_friend_requests(testUser, userDB) == expectedReturn
-#     except StopIteration:
-#         pass
-#     captured = capfd.readouterr()
-#     for r in responses:
-#         assert r in captured.out
-
-#     #assert that the friend request was actually removed
-
-def testIncomingFriendRequestAccept(monkeypatch, capfd):
+@pytest.mark.parametrize(
+    "mock_input,responses,expectedReturn",
+    [
+        (
+            ["y",""],  # mock input
+            [
+                "You are now friends with sillyBoi",  # responses
+            ],
+            None,  # expected return
+        ),
+        (
+            ["n",""],
+            [
+                "You have declined the friend request from sillyBoi",
+            ],
+            None,
+        ),
+        (
+            ["X",""],
+            [
+                "Invalid input. Please enter 'y' or 'n': ",
+            ],
+            None,
+        ),
+    ],
+    ids=["Accept", "Decline", "Invalid"],
+)
+def test_manageFriendRequest(mock_input, responses, expectedReturn, monkeypatch, capfd):
     userDB = UserDatabase()
     userDB.addUserDictList(fourAccounts)
     print(userDB.getUserDictList())
     testUser = userDB.getUser("dummy")
-    expectedReturn = None
-    mock_input = ["y","",]
+    # expectedReturn = None
     input_generator = iter(mock_input)
     monkeypatch.setattr("builtins.input", lambda _: next(input_generator))
-    responses = ["You are now friends with sillyBoi"]
+
     try:
         assert manage_friend_requests(testUser, userDB) == expectedReturn
     except StopIteration:
@@ -72,41 +50,4 @@ def testIncomingFriendRequestAccept(monkeypatch, capfd):
     for r in responses:
         assert r in captured.out
 
-def testIncomingFriendRequestDecline(monkeypatch, capfd):
-    userDB = UserDatabase()
-    userDB.addUserDictList(fourAccounts)
-    print(userDB.getUserDictList())
-    testUser = userDB.getUser("dummy")
-    expectedReturn = None
-    mock_input = ["n","",]
-    input_generator = iter(mock_input)
-    monkeypatch.setattr("builtins.input", lambda _: next(input_generator))
-    responses = ["You have declined the friend request from sillyBoi"]
-    try:
-        assert manage_friend_requests(testUser, userDB) == expectedReturn
-    except StopIteration:
-        pass
-    captured = capfd.readouterr()
-    print(captured.out)
-    for r in responses:
-        assert r in captured.out
-
-def testIncomingFriendRequestInvalidInput(monkeypatch, capfd):
-    userDB = UserDatabase()
-    userDB.addUserDictList(fourAccounts)
-    print(userDB.getUserDictList())
-    testUser = userDB.getUser("dummy")
-    expectedReturn = None
-    mock_input = ["z",""]
-    input_generator = iter(mock_input)
-    monkeypatch.setattr("builtins.input", lambda _: next(input_generator))
-    responses = ["Invalid input. Please enter 'y' or 'n': "]
-    try:
-        assert manage_friend_requests(testUser, userDB) == expectedReturn
-    except StopIteration:
-        pass
-    captured = capfd.readouterr()
-    print(captured.out)
-    
-    for r in responses:
-        assert r in captured.out
+    #assert that the friend request was actually removed
